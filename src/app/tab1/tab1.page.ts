33c2c4edfd6c8235cfe32a68cfd3a076b10f22ce
javiloria <34045912+javiloria@users.noreply.github.com>
2019-11-17 18:43:14 -0400

    acomodando error al consumir el api

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Component({
@@ -6,17 +6,24 @@ import { Observable } from 'rxjs';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
   constructor(private http: HttpClient) {  
   }
   pruebas=[]
 
-  getprueba() {
+  ngOnInit(
+    ) {
+     this.getPrueba();
+    }
+
+  getPrueba() {
       this.http.get("https://manos-que-dejan-huella.herokuapp.com/jugador").subscribe( 
         //TODO esto te devulve todos los jugadores hacer uno que te duvuelva solo un jugador /jugador
         (response: any)=>{    
           console.log(response)
+          if(response)
           this.pruebas = response;
+          else this.pruebas=["aaaa"]
         }
       );
   }
