e98a9498b6fcc9cfc11f36da0466705a232c6cf2
javiloria <34045912+javiloria@users.noreply.github.com>
2019-11-17 18:25:03 -0400

    consumiendo api desde front

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(private http: HttpClient) {  
+  }
+  pruebas=[]
+
+  getprueba() {
+      this.http.get("https://manos-que-dejan-huella.herokuapp.com/jugador").subscribe( 
+        //TODO esto te devulve todos los jugadores hacer uno que te duvuelva solo un jugador /jugador
+        (response: any)=>{    
+          console.log(response)
+          this.pruebas = response;
+        }
+      );
+  }
+
+  
+}
