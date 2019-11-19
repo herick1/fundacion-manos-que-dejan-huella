be34d9be5bf53f2f8be2b4134b358c4d7f95d4e7
herick1 <herick200@gmail.com>
2019-11-18 20:59:10 -0400

    agregando pages layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  //{ path: 'layout', loadChildren: './layout/layout.module#LayoutPageModule' }
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
