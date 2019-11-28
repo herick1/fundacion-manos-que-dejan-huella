c7c71dc506ed7aa89bac9f66f529fd7c8ba957b8
herick1 <herick200@gmail.com>
2019-11-27 21:30:37 -0400

    poniendo slide menu en quienes somos

--- src/app/quienes-somos/quienes-somos.page.ts
+++ src/app/quienes-somos/quienes-somos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-quienes-somos',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuienesSomosPage implements OnInit {
 
-  constructor() { }
+  constructor( public menuCtrl: MenuController) 
+  {
+  }
 
+toggleMenu() {
+    this.menuCtrl.toggle(); //Add this method to your button click function
+  }
   ngOnInit() {
   }
 
