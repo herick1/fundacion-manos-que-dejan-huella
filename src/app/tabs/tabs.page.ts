78fe4096b0669fd92a3f3cabbe7906897e6eb5ba
herick1 <herick200@gmail.com>
2019-11-27 20:26:42 -0400

    creando slide menu para aplicaciones moviles

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage {
+	public appPages = [
+	    {
+	      title: 'Card',
+	      url: '/home',
+	      icon: 'paper'
+	    },
+	    {
+	      title: 'Category',
+	      url: '/list',
+	      icon: 'walk',
+	      subPages: [{ title: 'subtest1', url: '' },
+	      { title: 'subtest2', url: '' },
+	      { title: 'subtest3', url: '' },
+	      { title: 'subtest4', url: '' },
+	      { title: 'subtest5', url: '' },
+	      { title: 'subtest6', url: '' },
+	      { title: 'subtest7', url: '' },
+	      { title: 'subtest8', url: '' }]
+	    }   
+	  ];
+
+  constructor( public menuCtrl: MenuController) 
+  {
+    //this.initializeApp();
+  }
+
+toggleMenu() {
+    this.menuCtrl.toggle(); //Add this method to your button click function
+  }
+
+}
+
