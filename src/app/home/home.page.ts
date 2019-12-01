1f1c4af93c72ccdd1dd8470002715b753c3f220c
herick1 <herick200@gmail.com>
2019-11-30 21:20:45 -0400

    fix: quienes somos y tabs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor( public menuCtrl: MenuController) 
+  {
   }
 
+toggleMenu() {
+    this.menuCtrl.toggle(); //Add this method to your button click function
+  }
+  ngOnInit() {
+  }
 }
