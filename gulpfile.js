8c3d9720cb6bc7a00a798dd1380205bb175e605d
javiloria <34045912+javiloria@users.noreply.github.com>
2019-12-05 15:34:24 -0400

    gzip

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var gzip = require('gulp-gzip');
-
-gulp.task('compress', function() {
-  gulp.src(['./dist/**/*.*'])
-      .pipe(gzip())
-      .pipe(gulp.dest('./dist'));
-});
