6833bec9b59a7d4e6c39ca53cae543ba2a2dcd0e
herick1 <herick200@gmail.com>
2019-12-05 23:20:50 -0400

    fix: menu en no-found

--- src/app/no-found/no-found.page.ts
+++ src/app/no-found/no-found.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-no-found',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./no-found.page.scss'],
 })
 export class NoFoundPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  focus:any;
+  focus1:any;
+  constructor( public menuCtrl: MenuController) 
+  {
   }
 
+toggleMenu() {
+    this.menuCtrl.toggle(); //Add this method to your button click function
+  }
+  ngOnInit() {
+  }
 }
