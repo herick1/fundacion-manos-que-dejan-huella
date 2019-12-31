2834992a963c21e54e6eb42b3636be8a9bd3a869
javiloria <34045912+javiloria@users.noreply.github.com>
2019-12-31 01:11:38 -0400

    arreglando vulnerabilidades

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ if(process.env.NODE_ENV === 'production') {
     if (req.header('x-forwarded-proto') !== 'https')
       res.redirect(`https://${req.header('host')}${req.url}`)
     else
-    express.static('www')
+    app.use(express.static('www'));
   })
 }
 //app.use(express.static('www'));
