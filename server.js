de7400c8b81fdcccf42bb3df449684259620d3bd
javiloria <34045912+javiloria@users.noreply.github.com>
2020-01-25 12:31:29 -0400

    probando falla del servidor

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ if(process.env.NODE_ENV === 'production') {
 
 app.use(express.static('www'));
 
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
