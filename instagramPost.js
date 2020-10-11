e4757f9bd887ac6e958dfd1500257c5c9fa7cfd9
Jorge Viloria <jorge.a.viloria@pwc.com>
2020-10-11 13:44:26 -0400

    cambiando respuesta a json

--- instagramPost.js
+++ instagramPost.js
@@ -66,22 +66,23 @@ const test = async username => {
   }
 
   try {
-    const url = `https://instagram.com/${username}`;
+		const url = `https://instagram.com/${username}`;
 		const {graphql: {user}} = await got(url, {searchParams: {__a: 1}}).json();
 		const email = getEmails(user.biography).values().next().value || '';
-    return {
-      ...user,
-      description: user.biography,
-      email,
-      followers: user.edge_followed_by.count,
-      following: user.edge_follow.count,
-      fullName: user.full_name,
-      posts: user.edge_owner_to_timeline_media.count,
-      url,
-      username,
-      website: user.external_url
-    };
-  } catch (error) {
+
+		return {
+			...user,
+			description: user.biography,
+			email,
+			followers: user.edge_followed_by.count,
+			following: user.edge_follow.count,
+			fullName: user.full_name,
+			posts: user.edge_owner_to_timeline_media.count,
+			url,
+			username,
+			website: user.external_url
+		};
+	}  catch (error) {
     console.log("Errorr");
     console.log(error);
     throw error;
